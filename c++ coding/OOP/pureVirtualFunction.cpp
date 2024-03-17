a35eb664934c9cb2cadececbd9e6bda141ea65a5
sakshamdev123 <sakshamdev9.com>
2024-03-18 00:03:34 +0530

    added pureVirtualFunction.cpp

---
+++ c++ coding/OOP/pureVirtualFunction.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Print
+{
+public:
+    virtual string getClassName() = 0;
+};
+
+class Entity : public Print
+{
+public:
+    virtual string getName() { return "AI"; }
+    string getClassName() override { return "Entity"; }
+};
+
+class Player : public Entity
+{
+private:
+    string name;
+
+public:
+    Player(const string &s) : name(s) {}
+    string getName() override { return name; }
+    string getClassName() override { return "Player"; }
+};
+
+void printName(Entity *e) { cout << e->getName() << endl; }
+void printObj(Print *p) { cout << p->getClassName() << endl; }
+
+int main()
+{
+    Entity *e = new Entity;
+    Player *p = new Player("ChatGPT");
+    printObj(e);
+    printObj(p);
+    delete e;
+    delete p;
+}
